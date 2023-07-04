23a62b4ec03e27ea3e0d6b279e1eeba21c726263
TeodorKoynov <teodor.koynov2005@gmail.com>
2023-07-04 10:22:54 +0300

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import colors from 'tailwindcss/colors';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,12 +9,41 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        inherit: colors.inherit,
+        current: colors.current,
+        transparent: colors.transparent,
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        red: colors.rose,
+        green: colors.teal,
+        background: '#111111',
+        highlight: {
+          100: '#FFEDD3',
+          200: '#FFE4BD',
+          300: '#FFDCA7',
+          400: '#FFD391',
+          500: '#FFC878',
+          600: '#FFB13C',
+          700: '#FC9700',
+          800: '#BD7100',
+          900: '#7E4B00',
+        },
+        brown: {
+          50: '#E1DBD9',
+          100: '#C2B6B4',
+          200: '#A4928E',
+          300: '#846F6A',
+          400: '#5E4F4C',
+          500: '#382F2D',
+          600: '#2F2826',
+          700: '#26201E',
+          800: '#1C1817',
+          900: '#13100F',
+        },
       },
     },
   },
   plugins: [],
-}
+};
